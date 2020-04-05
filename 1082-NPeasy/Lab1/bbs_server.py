956faa82e4dcf6a6b023a546f2b752b5a501def8
kaiiiz <at881005@gmail.com>
2020-04-05 11:05:24 +0000

    Organize code

--- 1082-NPeasy/Lab1/bbs_server.py
+++ 1082-NPeasy/Lab1/bbs_server.py
@@ -1,24 +1,28 @@
 import socket
 import threading
 import os
+import sys
 from dotenv import load_dotenv
 from bbs_db import BBS_DB
 
 load_dotenv()
 
+if len(sys.argv) == 2:
+    BBS_PORT = int(sys.argv[1])
+else:
+    BBS_PORT = int(os.getenv("BBS_SERVER_DEFAULT_PORT"))
+
 BBS_HOST = os.getenv("BBS_SERVER_IP")
-BBS_PORT = int(os.getenv("BBS_SERVER_PORT"))
-BBS_MAX_CLIENT = int(os.getenv("BBS_MAX_CLIENT"))
 DB_USERNAME = os.getenv("DB_ROOT_USERNAME")
 DB_PWD = os.getenv("DB_ROOT_PWD")
 
 
-class Shell():
-    def __init__(self, db):
-        self.db = db
+class Controller():
+    def __init__(self):
+        self.db = BBS_DB(DB_USERNAME, DB_PWD)
         self.user = None
 
-    def parse(self, cmd):
+    def execute(self, cmd):
         try:
             cmd = cmd.strip().decode()
         except:
@@ -29,9 +33,9 @@ class Shell():
         else:
             cmd_list = cmd.split()
 
-        return self.handle(cmd, cmd_list)
+        return self.parse(cmd, cmd_list)
 
-    def handle(self, cmd, cmd_list):
+    def parse(self, cmd, cmd_list):
         if cmd_list[0] == "register":
             return self.register_handler(cmd_list)
 
@@ -52,7 +56,7 @@ class Shell():
 
     def register_handler(self, cmd_list):
         if len(cmd_list) != 4:
-            return "usage: register <username> <email> <password>\n"
+            return "Usage: register <username> <email> <password>\n"
 
         username = cmd_list[1]
         email = cmd_list[2]
@@ -66,7 +70,7 @@ class Shell():
 
     def login_handler(self, cmd_list):
         if len(cmd_list) != 3:
-            return "usage: login <username> <password>\n"
+            return "Usage: login <username> <password>\n"
 
         username = cmd_list[1]
         password = cmd_list[2]
@@ -91,17 +95,17 @@ class Shell():
 
     def whoami_handler(self):
         if self.user:
-            return f"{self.user}.\n"
+            return f"{self.user}\n"
         else:
             return "Please login first.\n"
 
 
 class BBS(threading.Thread):
-    def __init__(self, socket, address, db):
+    def __init__(self, socket, address):
         threading.Thread.__init__(self)
         self.socket = socket
         self.address = address
-        self.shell = Shell(db)
+        self.controller = Controller()
 
     def run(self):
         print(f"New Connection.")
@@ -109,7 +113,7 @@ class BBS(threading.Thread):
         while True:
             self.socket.send(b"% ")
             cmd = self.socket.recv(1024)
-            ret = self.shell.parse(cmd)
+            ret = self.controller.execute(cmd)
             if ret == -1:
                 break
             if len(ret) > 0:
@@ -122,13 +126,11 @@ def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((BBS_HOST, BBS_PORT))
-    s.listen(BBS_MAX_CLIENT)
-
-    db = BBS_DB(DB_USERNAME, DB_PWD)
+    s.listen()
 
     while True:
         client_socket, client_addr = s.accept()
-        BBS(client_socket, client_addr, db).start()
+        BBS(client_socket, client_addr).start()
 
 
 if __name__ == "__main__":
