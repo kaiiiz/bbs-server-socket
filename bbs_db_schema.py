23cc1d9b7a2b8bf18891ec2e2f6a7d573c17bacb
kaiiiz <at881005@gmail.com>
2020-05-22 03:03:41 +0800

    schema cleaner

--- bbs_db_schema.py
+++ bbs_db_schema.py
@@ -1,3 +1,4 @@
+from db.base import Base
 from db.users import Users
 from db.boards import Boards
 from db.posts import Posts
@@ -5,10 +6,12 @@ from db.mails import Mails
 
 from constant import DB_HOST, DB_PORT, DB_USERNAME, DB_PWD
 from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 class SCHEMA_CREATOR:
     def __init__(self, host, port, username, pwd):
         self.engine = self.get_engine(host, port, username, pwd)
+        self.session_factory = sessionmaker(bind=self.engine)
 
     def get_engine(self, host, port, username, pwd):
         engine = create_engine(f"mysql://{username}:{pwd}@{host}:{port}")
@@ -21,6 +24,16 @@ class SCHEMA_CREATOR:
         Posts.__table__.create(bind=self.engine, checkfirst=True)
         Mails.__table__.create(bind=self.engine, checkfirst=True)
 
+    def clear_table(self):
+        session = self.session_factory()
+        session.execute('SET FOREIGN_KEY_CHECKS = 0')
+        for table in Base.metadata.tables:
+            session.execute(f'DROP TABLE {table};')
+        session.execute('SET FOREIGN_KEY_CHECKS = 1')
+        session.commit()
+        session.close()
+
 if __name__ == '__main__':
     creator = SCHEMA_CREATOR(DB_HOST, DB_PORT, DB_USERNAME, DB_PWD)
+    creator.clear_table()
     creator.create_table()
